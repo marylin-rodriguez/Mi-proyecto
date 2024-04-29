08521fa7e508f94afe62009945818f2be7d85d0c
marylin-rodriguez <159863356+marylin-rodriguez@users.noreply.github.com>
2024-04-28 21:03:27 -0600

    Add files via upload

---
+++ App.java
@@ -0,0 +1,16 @@
+package org.marylin;
+
+import javax.swing.*;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+       JFrame frame = new JFrame();
+       frame.setVisible(true);
+    }
+}
